4088c03016ca879c6124b8a84efb09a0b9bcc457
Jensi <jensichothani43.gmail.com>
2026-08-19 18:26:00 +0530

    Fix sitemap

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,28 +1,45 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+
+from blog.sitemaps import PostSitemap
+
+
+# Sitemap configuration
+sitemaps = {
+    "posts": PostSitemap,
+}
 
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
 
+    # Blog
     path("", include("blog.urls")),
 
+    # Login / Logout
     path(
         "accounts/",
         include("django.contrib.auth.urls"),
     ),
-]
 
+    # Sitemap
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 
-from django.views.static import serve
-from django.urls import re_path
-
-urlpatterns += [
+    # Media files
     re_path(
-        r'^media/(?P<path>.*)$',
+        r"^media/(?P<path>.*)$",
         serve,
-        {'document_root': settings.MEDIA_ROOT},
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
     ),
 ]
