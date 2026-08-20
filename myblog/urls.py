04f74feff7f307efd70af8edf8d3136762cb5260
jensichothani43-creator <jensichothani43@gmail.com>
2026-08-20 11:45:26 +0530

    Implement robots.txt view for SEO
    Added a view for serving robots.txt with sitemap information.

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,8 +3,21 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.http import HttpResponse
 
 from blog.sitemaps import PostSitemap
+
+
+def robots_txt(request):
+    content = (
+        "User-agent: *\n"
+        "Allow: /\n"
+        "Disallow: /admin/\n"
+        "Disallow: /accounts/\n"
+        "\n"
+        "Sitemap: https://blog-ak8g.onrender.com/sitemap.xml\n"
+    )
+    return HttpResponse(content, content_type="text/plain")
 
 
 # Sitemap configuration
@@ -33,6 +46,9 @@ urlpatterns = [
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
+
+    # Robots.txt
+    path("robots.txt", robots_txt),
 
     # Media files
     re_path(
